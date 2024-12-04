529466cf4cfbb16712882990aab9e0043242e43d
Dorianrst <102527310+Dorianrst@users.noreply.github.com>
2024-12-04 22:59:05 +0100

    Initial commit

---
+++ c_wire.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#fonction permettant d'aider l'utilisateur en cas d'erreur 
+function help{
+        echo"Chemin du fichier Preing 2 Info/C_wire.sh"
+        echo"Type de station: hvb, hva ,lv"
+        echo"Type de consommateur: comp(entreprise),indiv(particuliers),all(tous)"
+        echo"Interdiction : hvb all, hvb indiv, hva all , hva indiv"
+        echo"ID central"
+        echo"option aide: -h"
+        echo "-"
+}
+
+    if[-h]
