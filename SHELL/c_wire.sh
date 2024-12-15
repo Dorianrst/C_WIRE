d456cbcb3df2d91eca3232c481f7ca0c94f3e903
Dorianrst <102527310+Dorianrst@users.noreply.github.com>
2024-12-15 21:41:42 +0100

    SHELL

--- SHELL/c_wire.sh
+++ SHELL/c_wire.sh
@@ -19,51 +19,86 @@ function_help() {
 }
 #Function A function that checks that all the arguments entered by the user are correct and that no arguments are missing.
 argument_verifiaction() {
-
+    local file_path=$1
     local station_type=$2
     local consumer_type=$3
     local ID=$4
     #We have to change this for the final test with c-wire_v25.dat
-    local file_path=$(find .. -iname "c-wire_v00.dat") 2>/dev/null              #Cette ligne de commande permet de chercher le fichier c-wire.csv dans mes dossiers
-    local Graphs_path=$(find . -type d -name "Graphs" -print -quit) 2>/dev/null #Cette ligne de commande permet de chercher si le dosier Graphs dans mes dossiers
-    local Temps_path=$(find . -type d -name "Temp" -print -quit) 2>/dev/null    #Cette ligne de commande permet de chercher le dossier Temps dans mes dossiers
 
-    if [["$station_type" != "hvb" && "$station_type" != "hva" && "$station_type" != "lv"]]; then
-        echo "Error : Invalid value" >&2
+    if [ -f "data.dat" ]; then
+        echo "Researching the dat file"
+        echo "......................."
+        echo "The file .dat exist"
+    else
+        echo "The file doesn't exist"
+        echo "Please download the .dat file"
         function_help
-        exit 1
+        exit 0
     fi
 
-    if [["$consumer_type" != "comp" && "$consumer_type" != "indiv" && "all"]]; then
+    if [[ "$station_type" != "hvb" && "$station_type" != "hva" && "$station_type" != "lv" ]]; then
         echo "Error : Invalid value" >&2
         function_help
-        exit 1
+        echo "You forgot to select the stations you want to process, e.g.: hva, hvb, lv"
+        exit 0
     fi
 
-    if [-z "$ID" ]; then
+    if [[ "$consumer_type" != "comp" && "$consumer_type" != "indiv" && "all" ]]; then
+        echo "Error : Invalid value"
+        function_help
+        echo "You forgot to select the consumers you want to process: comp, indiv, all "
+        exit 0
+    fi
+
+    if [ -z "$ID" ]; then
         #Put a fonction to analise all the data
-        echo "Processing all the data"
-    elif ! [["$ID" =~ ^[0-9]+$]]; then
+        echo "The station ID was not specified at the time of processing, so we will process all the stations $station_type"
 
-        echo "Processing only the data of the station : $ID"
+    elif ["$ID" -ge 5 ] || ["$ID" -le 1 ]; then
+        echo "The ID of a central station is between 1 and 5"
+        function_help
+        exit 0
 
     else
         echo " Correct ID"
-    fi
-
-    #I check  if the c-wire_v25.dat exist
-    if [ -e "$file_path" ]; then
-        echo "The c-wire_v25.dat file exist"
-        echo "$file_path"
+        echo "Processing will be done only with the control unit $ID"
     fi
 }
 
 veriffication_for_executable() {
-    #Checking the .exe and compiling the C
-    if [ -f "$executable"]; then
-        echo "The  $executable already exist.Compilation of $executable ..."
+    source_file="c_wire.c"
+    executable="./c_wire"
+
+    # Check if the executable exists
+    if [ ! -x "$executable" ]; then
+        echo "The executable '$executable' does not exist. Attempt to compile..."
+
+        # Check if the source file exists
+        if [ ! -f "$source_file" ]; then
+            echo "Error: the source file '$source_file' cannot be found."
+            exit 1
+        fi
+
+        # Compiling the source file
+        gcc "$source_file" -o "$executable"
+        if [ $? -ne 0 ]; then
+            echo "Erreur : la compilation de '$source_file' a échoué."
+            exit 2
+        fi
+
+        echo "Compilation successful: '$executable' has been created."
+    fi
+
+    # Exécuter l'exécutable avec les arguments fournis au script
+    echo "Execution of '$executable' with the arguments: $@"
+    "$executable" "$@"
+
+    # Check that the run has been completed successfully
+    if [ $? -ne 0 ]; then
+        echo "Erreur : l'exécution de '$executable' a échoué."
+        exit 3
     else
-        echo "The $executable doesn't exist." >&2
+        echo 'The execution was completed successfully.'
     fi
 }
 
@@ -88,181 +123,3 @@ verification_temp_graph() {
         mkdir -p "temp"
     fi
 }
-filter() {
-
-    local output_file
-
-    #Initialisation of the start_time variable for calculating execution time
-    start_time=$(date +%s.%s)
-
-    #This condition is used to check whether I am passing an argument in station where there are three types of argument HVB / HVA / LV if there is no argument, I display that there is no argument and stop the program.
-    if [ $# -eq 0 ]; then
-        echo "No arguments provided"
-        function_help
-        exit 1
-    fi
-
-    #ORDER IN THE C-WIRE_v25.dat
-    #Power plant;HV-B Station;HV-A Station;LV Station;Company;Individual;Capacity;Load
-    #  1          2                 3          4         5         6        7       8
-
-    #Dans cette boucle on regarde l'ensemble des arguments et on lance le programme associée à l'argument mis en paramètre soit HVB / HVA / LV
-    for arg in "$@"; do
-        case "$arg" in
-        -hvb)
-            echo "Exécution du tri selon la colonne Station HVB"
-            cut -d';' -f2,5,6,7,8 c-wire.csv | sort -n -t';' -k1 >hvb.csv
-            echo "Le tri selon la colonne Station HVB a été exécuté"
-
-            #Lance la commande associé au fait de prendre que les colonnes suivantes  HVB / COMPANY / INDIVIDUAL / CAPACITY / LOAD et ressort un tableau nommé hvb.csv
-            #ORDRE DANS LE HBV.CSV
-            #HV-B Station;Company;Individual;Capacity;Load
-            #  1            2          3          4       5
-
-            #Lance une nouvelle boucle dans laquelle on regarde un nouvelle argument qui peut etre l'un des suivant COMPANY / INDIVIDUAL / ALL
-            for arg in "$@"; do
-                case "$arg" in
-                -comp)
-                    echo "Exécution du tri selon la colonne Company"
-                    cut -d';' -f1,2,4,5 hvb.csv | sort -n -t';' -k1 >temp_hvb_comp.csv
-                    echo "Le tri selon la colonne Station HVB et selon la company a été exécuté"
-                    #manque le calcul pour permettre de voir combien chaque HVB a pour capacité de production et combien les entreprises utilises dans cette productions ce fait grace à la commande awk
-                    ;;
-
-                -indiv)
-                    echo "Cette commande ne peut pas être appliqué"
-                    exit 1
-                    ;;
-                -all)
-                    echo "Cette commande ne peut pas être appliqué"
-                    exit 1
-                    ;;
-                esac
-            done
-            ;;
-
-        -hva)
-            echo "Exécution du tri selon la colonne Station HVA"
-            cut -d';' -f3,5,6,7,8 c-wire.csv | sort -n -t';' -k1 >hva.csv
-            echo "Le tri selon la colonne Station HVA a été exécuté"
-
-            # lance la commande associé au fait de prendre que les colonnes suivantes  HVA / COMPANY / INDIVIDUAL / CAPACITY / LOAD et ressort un tableau nommé hva.csv
-            #ORDRE DANS LE HVA.CSV
-            #HV-A Station;Company;Individual;Capacity;Load
-            #  1            2          3          4       5
-
-            #Lance une nouvelle boucle dans laquelle on regarde un nouvelle argument qui peut etre l'un des suivant COMPANY / INDIVIDUAL / ALL
-            for arg in "$@"; do
-                case "$arg" in
-                -comp)
-                    echo "Exécution du tri selon la colonne Company"
-                    cut -d';' -f1,2,4,5 hva.csv | sort -n -t';' -k1 >temp_hva_comp.csv
-                    echo "Le tri selon la colonne Station HVA et selon la company a été exécuté"
-                    #manque le calcul pour permettre de voir combien chaque HVA a pour capacité de production et combien les entreprises utilises dans cette productions ce fait grace à la commande awk
-                    ;;
-                -indiv)
-                    echo "Cette commande ne peut pas être appliqué"
-                    exit 1
-                    ;;
-                -all)
-                    echo "Cette commande ne peut pas être appliqué"
-                    exit 1
-                    ;;
-                esac
-            done
-            ;;
-
-        -lv)
-            echo "Exécution du tri selon la colonne Station LV"
-            cut -d';' -f4,5,6,7,8 c-wire.csv | sort -n -t';' -k1 >lv.csv
-            echo "Le tri selon la colonne Station LV a été exécuté"
-            # lance la commande associé au fait de prendre que les colonnes suivantes  LV / COMPANY / INDIVIDUAL / CAPACITY / LOAD et  ressort un tableau nommé lv.csv
-            #ORDRE DANS LE LV.CSV
-            #LV Station;Company;Individual;Capacity;Load
-            #  1            2          3          4       5
-
-            #Lance une nouvelle boucle dans laquelle on regarde un nouvelle argument qui peut etre l'un des suivant COMPANY / INDIVIDUAL / ALL
-            for arg in "$@"; do
-                case "$arg" in
-                -comp)
-                    echo "Exécution du tri selon la colonne Company"
-                    cut -d';' -f1,2,4,5 lv.csv | sort -n -t';' -k1 >temp_lv_comp.csv
-                    echo "Le tri selon la colonne Station HVA et selon la company a été exécuté"
-                    #manque le calcul pour permettre de voir combien chaque LV a pour capacité de production et combien les entreprises utilises dans cette productions ce fait grace à la commande awk
-                    ;;
-                -indiv)
-                    echo "Exécution du tri selon la colonne Individual"
-                    cut -d';' -f1,3,4,5 lv.csv | sort -n -t';' -k1 >temp_lv_indiv.csv
-                    echo "Le tri selon la colonne Station HVA et selon la colonne individual a été exécuté"
-                    #manque le calcul pour permettre de voir combien chaque LV a pour capacité de production et combien les entreprises utilises dans cette productions ce fait grace à la commande awk
-                    ;;
-                -all)
-                    echo "Exécution du tri selon la colonne Individual"
-                    cut -d';' -f1,2,3,4,5 lv.csv | sort -n -t';' -k1 >temp_lv_all.csv
-                    #manque le calcul pour permettre de voir combien chaque LV a pour capacité de production et combien les entreprises utilises dans cette productions ce fait grace au programme C
-                    ;;
-                esac
-            done
-            ;;
-
-        *)
-            echo "Argument inconnu : $arg"
-            ;; # Cas où on prends un autre arguments en paramètres cela ressort un message d'erreur
-        esac
-    done
-
-    # Calcul et affichage du temps écoulé
-    end_time=$(date +%s.%s)
-    elapsed_time=$(echo "$end_time - $start_time" | bc)
-    echo "Temps écoulé : $elapsed_time secondes"
-
-    #Running the C program
-    #verification_executable
-    fichier_source="c_wire.c"
-    executable="c_wire.exe"
-
-    if [ -f "$source_file" ]; then
-        gcc "$source_file" -o "$executable"
-        if [ $? -eq 0 ]; then
-            echo "Successful compilation. The $executable file has been created."
-        else
-            echo "Error: $source_file compilation failed." >&2
-        fi
-    else
-        echo "Error: The source file $source_file does not exist." >&2
-    fi
-
-    #Les arguments devront etre le fichier, la station choisi etc...
-    arg1=3
-    arg2=5
-    arg3=7
-
-    output=$(./$executable $arg1 $arg2 $arg3)
-    code_return=$?
-
-    # Verifier la sortie
-    if [ $code_return -eq 0 ]; then
-        echo "Le calcul a réussi. Résultat : $output"
-    else
-        echo "Erreur : Le programme a échoué avec le code $code_return."
-    fi
-}
-
-main() {
-    if [[ "$*" == *"-h"* ]]; then
-        function_help
-        exit 0
-    fi
-
-    local station_type=$2
-    local consumer_type=$3
-    local ID=$4
-    #We have to change this for the final test with c-wire_v25.dat
-    local file_path=$(find .. -iname "c-wire_v00.dat") 2>/dev/null              #Cette ligne de commande permet de chercher le fichier c-wire.csv dans mes dossiers
-    local Graphs_path=$(find . -type d -name "Graphs" -print -quit) 2>/dev/null #Cette ligne de commande permet de chercher si le dosier Graphs dans mes dossiers
-    local Temps_path=$(find . -type d -name "Temp" -print -quit) 2>/dev/null
-    argument_verifiaction "file_path" "sation_type" "consumer_type" "ID"
-    #verification_executable "$1"  ;,
-}
-
-main "$@"
